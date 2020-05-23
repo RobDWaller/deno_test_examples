1885e054bc07b1508f1fe7133258382b10727adb
RobDWaller <rdwaller1984@googlemail.com>
2020-05-23 13:25:47 +0100

    Added assert fails example tests.

--- tests/assertions.test.ts
+++ tests/assertions.test.ts
@@ -1,18 +1,50 @@
 import {
-  assert
+  assert,
+  assertThrows,
+  AssertionError,
 } from "https://deno.land/std/testing/asserts.ts";
 
 Deno.test("Assert Example", () => {
   assert(1);
+  assert(2);
   assert(true);
   assert("true");
   assert("false");
   assert("0");
   assert(-1);
 
-  class Foo{};
+  class Foo {}
   assert(new Foo());
 
-  const bar = function bar(){}
+  const bar = function bar() {};
   assert(bar);
+
+  assert({});
+  assert([]);
+});
+
+Deno.test("Assert Example Throws", () => {
+  assertThrows(() => {
+    assert(0);
+  }, AssertionError);
+  assertThrows(() => {
+    assert(false);
+  }, AssertionError);
+  assertThrows(() => {
+    assert("");
+  }, AssertionError);
+  assertThrows(() => {
+    assert(null);
+  }, AssertionError);
+  assertThrows(() => {
+    assert(undefined);
+  }, AssertionError);
+
+  assertThrows(
+    () => {
+      assert(false, "Value is falsy.");
+    },
+    AssertionError,
+    "Value is falsy.",
+  );
 });
